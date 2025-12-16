fdb9b9ad31444ad889719c7f2b61f7ffe9a445ea
Fatiha <elhabtifatiha07@gmail.com>
2025-12-16 15:10:40 +0100

    communication microservices reussi

---
+++ conference-service/src/main/java/ma/enset/conferenceservice/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package ma.enset.conferenceservice.config; // (Changez le package selon le service)
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll() // AUTORISER TOUT
+                );
+        return http.build();
+    }
+}
---
+++ gateway-service/src/main/java/ma/enset/gatewayservice/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package ma.enset.gatewayservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable) // Désactiver la protection CSRF pour les tests
+                .authorizeExchange(exchange -> exchange
+                        .anyExchange().permitAll() // AUTORISER TOUTES les requêtes
+                );
+        return http.build();
+    }
+}
